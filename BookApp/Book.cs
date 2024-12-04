44cfcd4a00aedf5ff8b4aa28cd368318d33fc557
SudhaPuchala <sudhavardhann@gmail.com>
2024-12-04 14:39:41 +0100

    Add project files.

---
+++ BookApp/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp
+{
+    internal class Book
+    {
+        public string Title;
+        public string Description;
+        public string Author;
+        public int Year;
+
+
+        public void Identity()
+        {
+            Console.WriteLine($"Title:{Title}");
+            Console.WriteLine($"Description:{Description}");
+            Console.WriteLine($"Year:{Year}");
+            Console.WriteLine($"Author:{Author}");
+        }
+
+    }
+}
---
+++ BookApp/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace BookApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Book Library");
+
+            List<Book> books = new List<Book>();
+            while (true) 
+            {
+                Console.WriteLine("Do you want to add a book? (yes/no):");
+                string response = Console.ReadLine();
+
+                if(response == "no")
+                    break;
+                Book book = new Book();
+
+                Console.WriteLine("Enter the title of the book." );
+                 book.Title = Console.ReadLine();
+
+                Console.WriteLine("Enter the author of the book.");
+                book.Author = Console.ReadLine();
+
+                Console.WriteLine("Enter the year of the book published.");
+                book.Year= int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the description of the book.");
+                book.Description = Console.ReadLine();
+
+                books.Add(book);
+                Console.WriteLine("Book added successfully!!"  );
+            }
+            Console.WriteLine("List of all books entered:"  );
+            foreach (Book book in books)
+            {
+                book.Identity();
+                Console.WriteLine(  );
+            }
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
